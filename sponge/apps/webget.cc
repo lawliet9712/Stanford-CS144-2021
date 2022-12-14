c7ebf206dd02563be4fa140da53d36fec4784900
lawliet <www.wwj19@qq.com>
2022-12-14 17:55:16 +0800

    lab0 finish webget

--- sponge/apps/webget.cc
+++ sponge/apps/webget.cc
@@ -1,14 +1,57 @@
 #include "socket.hh"
 #include "util.hh"
+#include "address.hh"
 
 #include <cstdlib>
 #include <iostream>
+#include <arpa/inet.h>
 
 using namespace std;
 
+/* c style implement
 void get_URL(const string &host, const string &path) {
     // Your code here.
+    // 1. init socket
+    struct hostent* h = gethostbyname(host.c_str());
+    if (h == nullptr)
+    {
+        cerr << "Function called: get_URL(" << host << ", " << path << ") Failed, not found hostname\n";
+        return;
+    }
 
+    cout << "Host Addr =" << h->h_name << " Ip=" << inet_ntoa(*((struct in_addr *)h->h_addr)) << "\n";
+
+    int sock = socket(AF_INET, SOCK_STREAM, 0);
+    struct sockaddr_in server_addr;
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_addr = *(struct in_addr*)h->h_addr;
+    server_addr.sin_port = htons(80);
+    
+    // 2. connect to host
+    if (connect(sock, reinterpret_cast<struct sockaddr*>(&server_addr), sizeof(server_addr)) == -1)
+    {
+        cerr << "Function called: get_URL(" << host << ", " << path << ") Failed, conncet failed\n";
+        return;
+    }
+    cout << "Connect finish ..." << "\n";
+
+    // 3. send request path
+    // extern ssize_t send (int __fd, const void *__buf, size_t __n, int __flags);
+    string buf = "GET " + path + " HTTP/1.1\r\n";
+    ssize_t send_size = send(sock, buf.c_str(), buf.size(), 0);
+    buf = "HOST: " + host + "\r\n";
+    send_size = send(sock, buf.c_str(), buf.size(), 0);
+    buf = "Connection: close\r\n";
+    send_size = send(sock, buf.c_str(), buf.size(), 0);
+    buf = "\r\n";
+    send_size = send(sock, buf.c_str(), buf.size(), 0);
+
+    // 4. read response data
+    char ch;
+    while (recv(sock, &ch, sizeof(ch), 0) != 0)
+    {
+        cout << ch;
+    }
     // You will need to connect to the "http" service on
     // the computer whose name is in the "host" string,
     // then request the URL path given in the "path" string.
@@ -17,8 +60,40 @@ void get_URL(const string &host, const string &path) {
     // (not just one call to read() -- everything) until you reach
     // the "eof" (end of file).
 
-    cerr << "Function called: get_URL(" << host << ", " << path << ").\n";
-    cerr << "Warning: get_URL() has not been implemented yet.\n";
+    //cerr << "Function called: get_URL(" << host << ", " << path << ").\n";
+    //cerr << "Warning: get_URL() has not been implemented yet.\n";
+}
+*/
+
+// should use TCPClient
+void get_URL(const string &host, const string &path) {
+    // Your code here.
+    // 1. init socket
+    TCPSocket socket;
+    
+    // 2. connect to host
+    socket.connect(Address(host, "http"));
+
+    // 3. send request path
+    // extern ssize_t send (int __fd, const void *__buf, size_t __n, int __flags);
+    socket.write("GET " + path + " HTTP/1.1\r\n");
+    socket.write("HOST: " + host + "\r\n");
+    socket.write("Connection: close\r\n");
+    socket.write("\r\n");
+
+    // 4. read response data
+    while(!socket.eof())
+        cout << socket.read();
+    // You will need to connect to the "http" service on
+    // the computer whose name is in the "host" string,
+    // then request the URL path given in the "path" string.
+
+    // Then you'll need to print out everything the server sends back,
+    // (not just one call to read() -- everything) until you reach
+    // the "eof" (end of file).
+
+    //cerr << "Function called: get_URL(" << host << ", " << path << ").\n";
+    //cerr << "Warning: get_URL() has not been implemented yet.\n";
 }
 
 int main(int argc, char *argv[]) {
